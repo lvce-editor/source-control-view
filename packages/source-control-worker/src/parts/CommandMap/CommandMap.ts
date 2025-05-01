4b11cf3979f4adccc1508a584b08a36f2fb0372e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-01 10:40:48 +0200

    feature: add initialize command (#2)

--- packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
+import * as Initialize from '../Initialize/Initialize.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
   'SourceControl.terminate': Terminate.terminate,
+  'Initialize.initialize': Initialize.initialize,
 }
