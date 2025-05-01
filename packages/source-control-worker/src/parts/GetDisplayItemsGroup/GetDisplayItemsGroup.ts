036b755cbc6e11b7f692ae257a8154b861fffbac
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-01 21:12:59 +0200

    feature: move displayItems into separate file (#59)

---
+++ packages/source-control-worker/src/parts/GetDisplayItemsGroup/GetDisplayItemsGroup.ts
@@ -0,0 +1,54 @@
+import type { DisplayItem } from '../DisplayItem/DisplayItem.ts'
+import * as DirentType from '../DirentType/DirentType.ts'
+import * as IconTheme from '../IconTheme/IconTheme.ts'
+import * as Workspace from '../Workspace/Workspace.ts'
+
+export const getDisplayItemsGroup = (group: any, isExpanded: boolean): readonly DisplayItem[] => {
+  const displayItems: DisplayItem[] = []
+  const { id, label, items } = group
+  if (!items) {
+    throw new Error('Source control group is missing an items property')
+  }
+  const length = items.length
+  const type = isExpanded ? DirentType.DirectoryExpanded : DirentType.Directory
+  const icon = isExpanded ? 'ChevronDown' : 'ChevronRight'
+  if (length > 0) {
+    displayItems.push({
+      file: '',
+      label,
+      detail: '',
+      posInSet: 1,
+      setSize: 1,
+      icon,
+      decorationIcon: '',
+      decorationIconTitle: '',
+      decorationStrikeThrough: false,
+      type,
+      badgeCount: length,
+      groupId: id,
+    })
+  }
+  if (isExpanded) {
+    for (let i = 0; i < length; i++) {
+      const item = items[i]
+      const { file, icon, iconTitle, strikeThrough } = item
+      const baseName = Workspace.pathBaseName(file)
+      const folderName = file.slice(0, -baseName.length - 1)
+      displayItems.push({
+        file,
+        label: baseName,
+        detail: folderName,
+        posInSet: i + 1,
+        setSize: length,
+        icon: IconTheme.getFileIcon({ name: file }),
+        decorationIcon: icon,
+        decorationIconTitle: iconTitle,
+        decorationStrikeThrough: strikeThrough,
+        type: DirentType.File,
+        badgeCount: 0,
+        groupId: id,
+      })
+    }
+  }
+  return displayItems
+}
