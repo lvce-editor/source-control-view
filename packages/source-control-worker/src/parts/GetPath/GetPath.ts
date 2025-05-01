65e416e574c51da112faff641755caad69fb387f
Le Vivilet <levivlet@pm.me>
2025-05-01 22:21:35 +0200

    lint

--- packages/source-control-worker/src/parts/GetPath/GetPath.ts
+++ packages/source-control-worker/src/parts/GetPath/GetPath.ts
@@ -1,4 +1,4 @@
-import { DisplayItem } from '../DisplayItem/DisplayItem.ts'
+import type { DisplayItem } from '../DisplayItem/DisplayItem.ts'
 
 export const getPath = (item: DisplayItem): string => {
   return item.file
--- packages/source-control-worker/src/parts/GetPaths/GetPaths.ts
+++ packages/source-control-worker/src/parts/GetPaths/GetPaths.ts
@@ -1,4 +1,4 @@
-import { DisplayItem } from '../DisplayItem/DisplayItem.ts'
+import type { DisplayItem } from '../DisplayItem/DisplayItem.ts'
 import { getPath } from '../GetPath/GetPath.ts'
 
 export const getPaths = (items: readonly DisplayItem[]): readonly string[] => {
