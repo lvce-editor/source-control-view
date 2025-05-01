be89d75f1426e6b3b9eb823ade56d5e7e03d5ce8
Le Vivilet <levivlet@pm.me>
2025-05-01 12:14:08 +0200

    feature: add command module

---
+++ packages/source-control-worker/src/parts/ExtensionHostCommand/ExtensionHostCommand.ts
@@ -0,0 +1,10 @@
+import * as ExtensionHostShared from '../ExecuteProvider/ExecuteProvider.ts'
+import * as ExtensionHostCommandType from '../ExtensionHostCommandType/ExtensionHostCommandType.js'
+
+export const executeCommand = (id: string, ...args: readonly any[]): Promise<void> => {
+  return ExtensionHostShared.executeProvider({
+    event: `onCommand:${id}`,
+    method: ExtensionHostCommandType.CommandExecute,
+    params: [id, ...args],
+  })
+}
