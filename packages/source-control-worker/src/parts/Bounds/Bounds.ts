a50441a0b7c2fd3a362327209a71c84f5b486174
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-01 12:31:14 +0200

    feature: add bounds module (#32)
    * feature: add bounds module
    
    * lint

---
+++ packages/source-control-worker/src/parts/Bounds/Bounds.ts
@@ -0,0 +1,16 @@
+const state = {
+  width: 0,
+  height: 0,
+}
+
+export const get = (): any => {
+  return {
+    width: state.width,
+    height: state.height,
+  }
+}
+
+export const set = (width: number, height: number): void => {
+  state.width = width
+  state.height = height
+}
