ef057c7d290f54ffc55d95834c19f0acf121cc00
Le Vivilet <levivlet@pm.me>
2025-05-01 10:43:18 +0200

    feature: add source control strings

---
+++ packages/source-control-worker/src/parts/SourceControlStrings/SourceControlStrings.ts
@@ -0,0 +1,81 @@
+import * as I18nString from '../I18NString/I18NString.ts'
+import * as UiStrings from '../UiStrings/UiStrings.ts'
+
+export const openChanges = () => {
+  return I18nString.i18nString(UiStrings.OpenChanges)
+}
+
+export const openFile = () => {
+  return I18nString.i18nString(UiStrings.OpenFile)
+}
+export const openFileHead = () => {
+  return I18nString.i18nString(UiStrings.OpenFileHead)
+}
+
+export const discardChanges = () => {
+  return I18nString.i18nString(UiStrings.DiscardChanges)
+}
+
+export const stageChanges = () => {
+  return I18nString.i18nString(UiStrings.StageChanges)
+}
+
+export const addToGitignore = () => {
+  return I18nString.i18nString(UiStrings.AddToGitignore)
+}
+
+export const revealInExplorerView = () => {
+  return I18nString.i18nString(UiStrings.RevealInExplorerView)
+}
+
+export const openContainingFolder = () => {
+  return I18nString.i18nString(UiStrings.OpenContainingFolder)
+}
+
+export const discardAll = () => {
+  return I18nString.i18nString(UiStrings.DiscardAll)
+}
+
+export const stageAll = () => {
+  return I18nString.i18nString(UiStrings.StageAll)
+}
+
+export const discard = () => {
+  return I18nString.i18nString(UiStrings.Discard)
+}
+
+export const stage = () => {
+  return I18nString.i18nString(UiStrings.Stage)
+}
+
+export const unstageAll = () => {
+  return I18nString.i18nString(UiStrings.UnstageAll)
+}
+
+export const unstage = () => {
+  return I18nString.i18nString(UiStrings.Unstage)
+}
+
+export const viewAsTree = () => {
+  return I18nString.i18nString(UiStrings.ViewAsTree)
+}
+
+export const createPullRequest = () => {
+  return I18nString.i18nString(UiStrings.CreatePullRequest)
+}
+
+export const commitAndPush = () => {
+  return I18nString.i18nString(UiStrings.CommitAndPush)
+}
+
+export const refresh = () => {
+  return I18nString.i18nString(UiStrings.Refresh)
+}
+
+export const messageEnterToCommitOnMaster = () => {
+  return I18nString.i18nString(UiStrings.MessageEnterToCommitOnMaster)
+}
+
+export const sourceControlInput = () => {
+  return I18nString.i18nString(UiStrings.SourceControlInput)
+}
