24cc295d77336a26d786993173d4a8a658369671
Le Vivilet <levivlet@pm.me>
2025-05-01 21:51:35 +0200

    feature: add badge test

---
+++ packages/source-control-worker/test/GetBadgeVirtualDom.test.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@jest/globals'
+import { getBadgeVirtualDom } from '../src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+
+test('getBadgeVirtualDom', () => {
+  const result = getBadgeVirtualDom('test-class', 5)
+  expect(result).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: 'Badge test-class',
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '5',
+    },
+  ])
+})
+
+test('getBadgeVirtualDom with zero count', () => {
+  const result = getBadgeVirtualDom('test-class', 0)
+  expect(result).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: 'Badge test-class',
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '0',
+    },
+  ])
+})
+
+test('getBadgeVirtualDom with empty className', () => {
+  const result = getBadgeVirtualDom('', 5)
+  expect(result).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: 'Badge ',
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '5',
+    },
+  ])
+})
