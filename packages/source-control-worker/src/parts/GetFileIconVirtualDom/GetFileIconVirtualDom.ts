075aed99fac64ed223fcda8d76cef3301f2f760d
Le Vivilet <levivlet@pm.me>
2025-05-01 10:48:27 +0200

    feature: add icon dom

---
+++ packages/source-control-worker/src/parts/GetFileIconVirtualDom/GetFileIconVirtualDom.ts
@@ -0,0 +1,14 @@
+import { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getFileIconVirtualDom = (icon: string): VirtualDomNode => {
+  return {
+    type: VirtualDomElements.Img,
+    className: ClassNames.FileIcon,
+    src: icon,
+    role: AriaRoles.None,
+    childCount: 0,
+  }
+}
