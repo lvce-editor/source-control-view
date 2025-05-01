a056766aa9dff0393b363d202fedd1728a7bb423
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-01 21:58:17 +0200

    feature: add initialize test (#76)
    * feature: add initialize test
    
    * test
    
    * lint

---
+++ packages/source-control-worker/test/Initialize.test.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import { initialize } from '../src/parts/Initialize/Initialize.ts'
+import * as RpcId from '../src/parts/RpcId/RpcId.ts'
+import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
+
+test.skip('initialize', async () => {
+  // @ts-ignore
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invokeAndTransfer: (method: string, ...params: any[]) => {
+      return Promise.resolve()
+    },
+  })
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+  await initialize()
+  const actual = RpcRegistry.get(RpcId.ExtensionHostWorker)
+  expect(actual).toBeDefined()
+})
