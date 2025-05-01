fa7f1a591dafd9c3b2dbd3f93b5c92d7b8ef0623
Le Vivilet <levivlet@pm.me>
2025-05-01 10:49:35 +0200

    lint

--- packages/source-control-worker/src/parts/SourceControlStrings/SourceControlStrings.ts
+++ packages/source-control-worker/src/parts/SourceControlStrings/SourceControlStrings.ts
@@ -1,81 +1,81 @@
 import * as I18nString from '../I18NString/I18NString.ts'
 import * as UiStrings from '../UiStrings/UiStrings.ts'
 
-export const openChanges = () => {
+export const openChanges = (): string => {
   return I18nString.i18nString(UiStrings.OpenChanges)
 }
 
-export const openFile = () => {
+export const openFile = (): string => {
   return I18nString.i18nString(UiStrings.OpenFile)
 }
-export const openFileHead = () => {
+export const openFileHead = (): string => {
   return I18nString.i18nString(UiStrings.OpenFileHead)
 }
 
-export const discardChanges = () => {
+export const discardChanges = (): string => {
   return I18nString.i18nString(UiStrings.DiscardChanges)
 }
 
-export const stageChanges = () => {
+export const stageChanges = (): string => {
   return I18nString.i18nString(UiStrings.StageChanges)
 }
 
-export const addToGitignore = () => {
+export const addToGitignore = (): string => {
   return I18nString.i18nString(UiStrings.AddToGitignore)
 }
 
-export const revealInExplorerView = () => {
+export const revealInExplorerView = (): string => {
   return I18nString.i18nString(UiStrings.RevealInExplorerView)
 }
 
-export const openContainingFolder = () => {
+export const openContainingFolder = (): string => {
   return I18nString.i18nString(UiStrings.OpenContainingFolder)
 }
 
-export const discardAll = () => {
+export const discardAll = (): string => {
   return I18nString.i18nString(UiStrings.DiscardAll)
 }
 
-export const stageAll = () => {
+export const stageAll = (): string => {
   return I18nString.i18nString(UiStrings.StageAll)
 }
 
-export const discard = () => {
+export const discard = (): string => {
   return I18nString.i18nString(UiStrings.Discard)
 }
 
-export const stage = () => {
+export const stage = (): string => {
   return I18nString.i18nString(UiStrings.Stage)
 }
 
-export const unstageAll = () => {
+export const unstageAll = (): string => {
   return I18nString.i18nString(UiStrings.UnstageAll)
 }
 
-export const unstage = () => {
+export const unstage = (): string => {
   return I18nString.i18nString(UiStrings.Unstage)
 }
 
-export const viewAsTree = () => {
+export const viewAsTree = (): string => {
   return I18nString.i18nString(UiStrings.ViewAsTree)
 }
 
-export const createPullRequest = () => {
+export const createPullRequest = (): string => {
   return I18nString.i18nString(UiStrings.CreatePullRequest)
 }
 
-export const commitAndPush = () => {
+export const commitAndPush = (): string => {
   return I18nString.i18nString(UiStrings.CommitAndPush)
 }
 
-export const refresh = () => {
+export const refresh = (): string => {
   return I18nString.i18nString(UiStrings.Refresh)
 }
 
-export const messageEnterToCommitOnMaster = () => {
+export const messageEnterToCommitOnMaster = (): string => {
   return I18nString.i18nString(UiStrings.MessageEnterToCommitOnMaster)
 }
 
-export const sourceControlInput = () => {
+export const sourceControlInput = (): string => {
   return I18nString.i18nString(UiStrings.SourceControlInput)
 }
