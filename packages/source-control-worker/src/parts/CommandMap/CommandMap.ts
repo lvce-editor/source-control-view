0b5a2997c648a53a6ec367662380d3ce2473338b
Le Vivilet <levivlet@pm.me>
2025-05-01 12:44:36 +0200

    feature: register diff command

--- packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,4 +1,5 @@
 import * as Create2 from '../Create2/Create2.ts'
+import * as Diff2 from '../Diff2/Diff2.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
 import * as HandleButtonClick from '../HandleButtonClick/HandleButtonClick.ts'
 import * as HandleContextMenu from '../HandleContextMenu/HandleContextMenu.ts'
@@ -11,6 +12,7 @@ import * as Terminate from '../Terminate/Terminate.ts'
 export const commandMap = {
   'Initialize.initialize': Initialize.initialize,
   'SourceControl.handleContextMenu': WrapCommand.wrapCommand(HandleContextMenu.handleContextMenu),
+  'SourceControl.diff2': Diff2.diff2,
   'SourceControl.handleButtonClick': WrapCommand.wrapCommand(HandleButtonClick.handleButtonClick),
   'SourceControl.loadControl': LoadContent.loadContent,
   'SourceControl.terminate': Terminate.terminate,
