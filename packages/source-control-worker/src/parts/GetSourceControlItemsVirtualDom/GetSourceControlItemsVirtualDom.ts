96069ac60e3d4542e2f8fe116ce09f2dbe9adef9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-01 21:28:51 +0200

    feature: move source control header into separate file (#62)

--- packages/source-control-worker/src/parts/GetSourceControlItemsVirtualDom/GetSourceControlItemsVirtualDom.ts
+++ packages/source-control-worker/src/parts/GetSourceControlItemsVirtualDom/GetSourceControlItemsVirtualDom.ts
@@ -1,32 +1,14 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetSourceControlHeaderVirtualDom from '../GetSourceControlHeaderVirtualDom/GetSourceControlHeaderVirtualDom.ts'
 import * as GetSourceControlItemVirtualDom from '../GetSourceControlItemVirtualDom/GetSourceControlItemVirtualDom.ts'
 import * as GetSplitButtonVirtualDom from '../GetSplitButtonVirtualDom/GetSplitButtonVirtualDom.ts'
-import * as ViewletSourceControlStrings from '../SourceControlStrings/SourceControlStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getSourceControlItemsVirtualDom = (items: readonly any[], splitButtonEnabled: boolean): readonly VirtualDomNode[] => {
   const dom = []
-  dom.push(
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.SourceControlHeader,
-      childCount: 1,
-    },
-    {
-      type: VirtualDomElements.Input,
-      className: ClassNames.InputBox,
-      spellcheck: false,
-      autocapitalize: 'off',
-      autocorrect: 'off',
-      placeholder: ViewletSourceControlStrings.messageEnterToCommitOnMaster(),
-      ariaLabel: ViewletSourceControlStrings.sourceControlInput(),
-      childCount: 0,
-      onInput: 'handleInput',
-      onFocus: 'handleFocus',
-    },
-  )
+  dom.push(...GetSourceControlHeaderVirtualDom.getSourceControlHeaderVirtualDom())
   if (splitButtonEnabled) {
     const hasItems = items.length > 0
     dom.push(...GetSplitButtonVirtualDom.getSourceControlItemsVirtualDom(hasItems, 'Commit'))
