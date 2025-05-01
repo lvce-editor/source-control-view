ff38b174157dae2fbd314d02c8c3f0921f2a8e0d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-01 12:50:29 +0200

    feature: add renderaction command (#44)

--- packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
@@ -5,6 +5,7 @@ import * as HandleButtonClick from '../HandleButtonClick/HandleButtonClick.ts'
 import * as HandleContextMenu from '../HandleContextMenu/HandleContextMenu.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
+import * as RenderActions2 from '../RenderActions2/RenderActions2.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
 import * as WrapCommand from '../SourceControlStates/SourceControlStates.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
@@ -12,6 +13,7 @@ import * as Terminate from '../Terminate/Terminate.ts'
 export const commandMap = {
   'Initialize.initialize': Initialize.initialize,
   'SourceControl.handleContextMenu': WrapCommand.wrapCommand(HandleContextMenu.handleContextMenu),
+  'SourceControl.renderActions2': RenderActions2.renderActions,
   'SourceControl.diff2': Diff2.diff2,
   'SourceControl.handleButtonClick': WrapCommand.wrapCommand(HandleButtonClick.handleButtonClick),
   'SourceControl.loadControl': LoadContent.loadContent,
