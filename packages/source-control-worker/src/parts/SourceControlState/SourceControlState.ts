e8a4548d7cb0a8e6d48cdbc7efb5deab7c0719a3
Le Vivilet <levivlet@pm.me>
2025-05-01 11:10:56 +0200

    feature: add state interface

---
+++ packages/source-control-worker/src/parts/SourceControlState/SourceControlState.ts
@@ -0,0 +1,20 @@
+export interface SourceControlState {
+  readonly id: number
+  readonly itemHeight: number
+  readonly height: number
+  readonly minimumSliderSize: number
+  readonly allGroups: readonly any[]
+  readonly gitRoot: string
+  readonly items: readonly any[]
+  readonly enabledProviderIds: readonly string[]
+  readonly isExpanded: boolean
+  readonly buttons: readonly any[]
+  readonly root: string
+  readonly maxLineY: number
+  readonly scrollBarHeight: number
+  readonly finalDeltaY: number
+  readonly splitButtonEnabled: boolean
+  readonly providerId: string
+  readonly buttonIndex: number
+  readonly workspacePath: string
+}
