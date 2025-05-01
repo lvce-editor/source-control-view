1d4115abfecca4adba282c7f3bec73faa255fffc
Le Vivilet <levivlet@pm.me>
2025-05-01 21:11:06 +0200

    feature: add displayitem type

--- packages/source-control-worker/src/parts/SourceControlState/SourceControlState.ts
+++ packages/source-control-worker/src/parts/SourceControlState/SourceControlState.ts
@@ -1,35 +1,37 @@
+import type { DisplayItem } from '../DisplayItem/DisplayItem.ts'
+
 export interface SourceControlState {
   readonly allGroups: readonly any[]
   readonly buttonIndex: number
   readonly buttons: readonly any[]
-  readonly displayItems: readonly any[]
+  readonly deltaY: number
+  readonly displayItems: readonly DisplayItem[]
   readonly enabledProviderIds: readonly string[]
+  readonly fileIconCache: any
   readonly finalDeltaY: number
   readonly gitRoot: string
+  readonly handleOffset: number
   readonly height: number
+  readonly icons: readonly string[]
   readonly id: number
+  readonly index: readonly any[]
   readonly inputValue: string
   readonly isExpanded: boolean
   readonly itemHeight: number
   readonly items: readonly any[]
   readonly maxLineY: number
+  readonly merge: readonly any[]
   readonly minimumSliderSize: number
+  readonly minLineY: number
   readonly providerId: string
   readonly root: string
+  readonly scrollBarActive: boolean
   readonly scrollBarHeight: number
   readonly splitButtonEnabled: boolean
+  readonly untracked: readonly any[]
+  readonly width: number
+  readonly workingTree: readonly any[]
   readonly workspacePath: string
   readonly x: number
   readonly y: number
-  readonly width: number
-  readonly deltaY: number
-  readonly minLineY: number
-  readonly fileIconCache: any
-  readonly icons: readonly string[]
-  readonly handleOffset: number
-  readonly scrollBarActive: boolean
-  readonly merge: readonly any[]
-  readonly index: readonly any[]
-  readonly untracked: readonly any[]
-  readonly workingTree: readonly any[]
 }
