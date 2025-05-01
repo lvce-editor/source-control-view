18f0aacaaf95d03df99fbcbd99f2540165a7de2c
Le Vivilet <levivlet@pm.me>
2025-05-01 21:57:15 +0200

    test

--- packages/source-control-worker/test/Initialize.test.ts
+++ packages/source-control-worker/test/Initialize.test.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '@jest/globals'
 import { initialize } from '../src/parts/Initialize/Initialize.ts'
-import { MockRpc } from '../src/parts/MockRpc/MockRpc.ts'
 import * as RpcId from '../src/parts/RpcId/RpcId.ts'
 import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
+import { MockRpc } from '@lvce-editor/rpc'
 
 test.skip('initialize', async () => {
-  const mockRpc = await MockRpc.create({
+  // @ts-ignore
+  const mockRpc = MockRpc.create({
     commandMap: {},
     invokeAndTransfer: (method: string, ...params: any[]) => {
       return Promise.resolve()
