cc223968296ff614a820670a72be8b349d4d2246
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 00:34:31 +0200

    feature: move buttons into separate file (#118)

--- packages/source-control-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/source-control-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,28 +1,15 @@
-import type { DisplayItem } from '../DisplayItem/DisplayItem.ts'
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
 import { getDisplayItems } from '../GetDisplayItems/GetDisplayItems.ts'
 import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 import * as GetFinalDeltaY from '../GetFinalDeltaY/GetFinalDeltaY.ts'
 import { getGroups } from '../GetGroups/GetGroups.ts'
 import { getListHeight } from '../GetListHeight/GetListHeight.ts'
+import { getNewButtons } from '../GetNewButtons/GetNewButtons.ts'
 import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.ts'
 import * as GetProtocol from '../GetProtocol/GetProtocol.ts'
 import * as Preferences from '../Preferences/Preferences.ts'
 import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
 import * as SourceControl from '../SourceControl/SourceControl.ts'
-import * as SourceControlActions from '../SourceControlActions/SourceControlActions.ts'
-
-const getNewButtons = async (displayItems: readonly DisplayItem[], providerId: string, buttonIndex: number): Promise<readonly any[]> => {
-  if (buttonIndex === -1) {
-    return []
-  }
-  const item = displayItems[buttonIndex]
-  if (!item) {
-    return []
-  }
-  const actions = await SourceControlActions.getSourceControlActions(providerId, item.groupId, item.type)
-  return actions
-}
 
 export const loadContent = async (state: SourceControlState): Promise<SourceControlState> => {
   const { itemHeight, height, minimumSliderSize, workspacePath, fileIconCache } = state
