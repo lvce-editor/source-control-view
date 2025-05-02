e8797275a66b5fced7b8aa2706c00d2cca303bb9
Le Vivilet <levivlet@pm.me>
2025-05-02 17:46:19 +0200

    feature: move list dom into separate file

--- packages/source-control-worker/src/parts/GetSourceControlListVirtualDom/GetSourceControlListVirtualDom.ts
+++ packages/source-control-worker/src/parts/GetSourceControlListVirtualDom/GetSourceControlListVirtualDom.ts
@@ -1,16 +1,12 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as GetSourceControlHeaderVirtualDom from '../GetSourceControlHeaderVirtualDom/GetSourceControlHeaderVirtualDom.ts'
 import * as GetSourceControlItemVirtualDom from '../GetSourceControlItemVirtualDom/GetSourceControlItemVirtualDom.ts'
-import * as GetSplitButtonVirtualDom from '../GetSplitButtonVirtualDom/GetSplitButtonVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const getSourceControlItemsVirtualDom = (items: readonly any[], splitButtonEnabled: boolean): readonly VirtualDomNode[] => {
-  const hasItems = items.length > 0
+export const getSourceControlListVirtualDom = (items: readonly VisibleItem[]): readonly VirtualDomNode[] => {
   return [
-    ...GetSourceControlHeaderVirtualDom.getSourceControlHeaderVirtualDom(),
-    ...GetSplitButtonVirtualDom.getSplitButtonVirtualDom(hasItems, splitButtonEnabled, 'Commit'),
     {
       type: VirtualDomElements.Div,
       className: ClassNames.SourceControlItems,
