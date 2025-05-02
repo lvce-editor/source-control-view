c74ce365ec70a16a34617de853c71da43c8de141
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 00:20:33 +0200

    feature: add getButtonDom function (#116)

---
+++ packages/source-control-worker/src/parts/GetButtonVirtualDom/GetButtonVirtualDom.ts
@@ -0,0 +1,19 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { ActionButton } from '../ActionButton/ActionButton.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetIconVirtualDom from '../GetIconVirtualDom/GetIconVirtualDom.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getButtonVirtualDom = (button: ActionButton): readonly VirtualDomNode[] => {
+  const { icon, label } = button
+  return [
+    {
+      type: VirtualDomElements.Button,
+      className: ClassNames.SourceControlButton,
+      title: label,
+      ariaLabel: label,
+      childCount: 1,
+    },
+    GetIconVirtualDom.getIconVirtualDom(icon, VirtualDomElements.Span),
+  ]
+}
---
+++ packages/source-control-worker/src/parts/GetButtonsVirtualDom/GetButtonsVirtualDom.ts
@@ -0,0 +1,19 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { ActionButton } from '../ActionButton/ActionButton.ts'
+import * as EmptySourceControlButtons from '../EmptySourceControlButtons/EmptySourceControlButton.ts'
+import { getButtonVirtualDom } from '../GetButtonVirtualDom/GetButtonVirtualDom.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getButtonsVirtualDom = (buttons: readonly ActionButton[]): readonly VirtualDomNode[] => {
+  if (buttons === EmptySourceControlButtons.emptySourceControlButtons) {
+    return []
+  }
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'SourceControlButtons',
+      childCount: buttons.length,
+    },
+    ...buttons.flatMap(getButtonVirtualDom),
+  ]
+}
