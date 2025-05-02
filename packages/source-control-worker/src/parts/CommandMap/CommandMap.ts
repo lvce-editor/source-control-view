4ce7e6cccb7bf8431761f35eaaf76a85b7fbdae8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-02 18:32:32 +0200

    feature: add handleMouseOver command (#100)
    * feature: add handleMouseOver command
    
    * tsc
    
    * lint

--- packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
@@ -5,6 +5,7 @@ import * as HandleButtonClick from '../HandleButtonClick/HandleButtonClick.ts'
 import * as HandleClickAt from '../HandleClickAt/HandleClickAt.ts'
 import * as HandleContextMenu from '../HandleContextMenu/HandleContextMenu.ts'
 import * as HandleFocus from '../HandleFocus/HandleFocus.ts'
+import * as HandleMouseOver from '../HandleMouseOver/HandleMouseOver.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as Render2 from '../Render2/Render2.ts'
@@ -22,15 +23,16 @@ export const commandMap = {
   'SourceControl.diff2': Diff2.diff2,
   'SourceControl.getCommandIds': GetCommandIds.getCommandIds,
   'SourceControl.handleButtonClick': WrapCommand.wrapCommand(HandleButtonClick.handleButtonClick),
-  'SourceControl.selectIndex': WrapCommand.wrapCommand(SelectIndex.selectIndex),
   'SourceControl.handleClickAt': WrapCommand.wrapCommand(HandleClickAt.handleClickAt),
   'SourceControl.handleContextMenu': WrapCommand.wrapCommand(HandleContextMenu.handleContextMenu),
+  'SourceControl.handleMouseOver': WrapCommand.wrapCommand(HandleMouseOver.handleMouseOver),
   'SourceControl.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
   'SourceControl.handleFocus': WrapCommand.wrapCommand(HandleFocus.handleFocus),
   'SourceControl.render2': Render2.render2,
   'SourceControl.renderActions2': RenderActions2.renderActions,
   'SourceControl.renderEventListeners': RenderEventListeners.renderEventListeners,
   'SourceControl.saveState': SaveState.saveState,
+  'SourceControl.selectIndex': WrapCommand.wrapCommand(SelectIndex.selectIndex),
   'SourceControl.terminate': Terminate.terminate,
   'SourceControl.updateIcons': WrapCommand.wrapCommand(UpdateIcons.updateIcons),
 }
