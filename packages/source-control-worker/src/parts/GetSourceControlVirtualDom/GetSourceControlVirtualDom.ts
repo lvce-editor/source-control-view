bf3765345a265e94c553ddf062e36a00aa308673
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-02 17:47:11 +0200

    feature: move list dom into separate file (#89)

--- packages/source-control-worker/src/parts/GetSourceControlVirtualDom/GetSourceControlVirtualDom.ts
+++ packages/source-control-worker/src/parts/GetSourceControlVirtualDom/GetSourceControlVirtualDom.ts
@@ -2,11 +2,14 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import * as GetSourceControlItemsVirtualDom from '../GetSourceControlItemsVirtualDom/GetSourceControlItemsVirtualDom.ts'
+import * as GetSourceControlHeaderVirtualDom from '../GetSourceControlHeaderVirtualDom/GetSourceControlHeaderVirtualDom.ts'
+import * as GetSourceControlListVirtualDom from '../GetSourceControlListVirtualDom/GetSourceControlListVirtualDom.ts'
+import * as GetSplitButtonVirtualDom from '../GetSplitButtonVirtualDom/GetSplitButtonVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getSourceControlVirtualDom = (items: readonly VisibleItem[], splitButtonEnabled: boolean): readonly VirtualDomNode[] => {
+  const hasItems = items.length > 0
   const dom = [
     {
       type: VirtualDomElements.Div,
@@ -19,7 +22,9 @@ export const getSourceControlVirtualDom = (items: readonly VisibleItem[], splitB
       onWheel: DomEventListenerFunctions.HandleWheel,
       childCount: splitButtonEnabled ? 3 : 2,
     },
-    ...GetSourceControlItemsVirtualDom.getSourceControlItemsVirtualDom(items, splitButtonEnabled),
+    ...GetSourceControlHeaderVirtualDom.getSourceControlHeaderVirtualDom(),
+    ...GetSplitButtonVirtualDom.getSplitButtonVirtualDom(hasItems, splitButtonEnabled, 'Commit'),
+    ...GetSourceControlListVirtualDom.getSourceControlListVirtualDom(items),
   ]
   return dom
 }
