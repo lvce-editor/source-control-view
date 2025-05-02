49b46dff041bc4a2b98231437f9ba2a3da1c2d7d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 00:18:19 +0200

    feature: add item type (#115)

--- packages/source-control-worker/src/parts/GetSourceControlItemVirtualDom/GetSourceControlItemVirtualDom.ts
+++ packages/source-control-worker/src/parts/GetSourceControlItemVirtualDom/GetSourceControlItemVirtualDom.ts
@@ -1,9 +1,10 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 import { createItemDirectory } from '../CreateItemDirectory/CreateItemDirectory.ts'
 import { createItemOther } from '../CreateItemOther/CreateItemOther.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 
-export const getSourceControlItemVirtualDom = (item: any): readonly VirtualDomNode[] => {
+export const getSourceControlItemVirtualDom = (item: VisibleItem): readonly VirtualDomNode[] => {
   switch (item.type) {
     case DirentType.DirectoryExpanded:
     case DirentType.Directory:
