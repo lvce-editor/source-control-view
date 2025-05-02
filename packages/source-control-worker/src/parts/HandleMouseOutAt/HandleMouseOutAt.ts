df03f3dc2f68dbfe5ea49ef811d70b8f60969fb6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 00:03:37 +0200

    feature: improve mouseoutat function (#111)
    * feature: improve mouseoutat function
    
    * lint

--- packages/source-control-worker/src/parts/HandleMouseOutAt/HandleMouseOutAt.ts
+++ packages/source-control-worker/src/parts/HandleMouseOutAt/HandleMouseOutAt.ts
@@ -1,6 +1,8 @@
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
+import { getIndex } from '../GetIndex/GetIndex.ts'
 import { handleMouseOut } from '../HandleMouseOut/HandleMouseOut.ts'
 
-export const handleMouseOutAt = (state: SourceControlState, index: number): SourceControlState => {
-  return handleMouseOut(state, -1)
+export const handleMouseOutAt = (state: SourceControlState, eventX: number, eventY: number): SourceControlState => {
+  const index = getIndex(state, eventX, eventY)
+  return handleMouseOut(state, index)
 }
