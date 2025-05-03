045108d8d3a85e173bf93a6315b0d60eb6cb950f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 23:26:33 +0200

    feature: add header dom test (#153)

---
+++ packages/source-control-worker/test/GetSourceControlHeaderVirtualDom.test.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@jest/globals'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as GetSourceControlHeaderVirtualDom from '../src/parts/GetSourceControlHeaderVirtualDom/GetSourceControlHeaderVirtualDom.ts'
+import * as InputName from '../src/parts/InputName/InputName.ts'
+import * as ViewletSourceControlStrings from '../src/parts/SourceControlStrings/SourceControlStrings.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+
+test('getSourceControlHeaderVirtualDom', () => {
+  const result = GetSourceControlHeaderVirtualDom.getSourceControlHeaderVirtualDom()
+  expect(result).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.SourceControlHeader,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Input,
+      className: ClassNames.InputBox,
+      ariaLabel: ViewletSourceControlStrings.sourceControlInput(),
+      autocapitalize: 'off',
+      autocorrect: 'off',
+      childCount: 0,
+      name: InputName.SourceControlInput,
+      onFocus: DomEventListenerFunctions.HandleFocus,
+      onInput: DomEventListenerFunctions.HandleInput,
+      placeholder: ViewletSourceControlStrings.messageEnterToCommitOnMaster(),
+      spellcheck: false,
+    },
+  ])
+})
