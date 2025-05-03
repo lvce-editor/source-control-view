f2f37c1e155cc47db37ac6edb73c7d016b99af36
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 14:44:59 +0200

    feature: use plainMessagePort rpc to avoid needing to start port manually (#137)

--- packages/source-control-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
+++ packages/source-control-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
@@ -2,6 +2,6 @@ import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
 export const sendMessagePortToExtensionHostWorker = async (port: MessagePort): Promise<void> => {
-  const command = 'HandleMessagePort.handleMessagePort'
+  const command = 'HandleMessagePort.handleMessagePort2'
   await ParentRpc.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker', port, command, RpcId.DebugWorker)
 }
