49f53f9cfcf809df4d1b7281a0b7d6c43bb67e8b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 22:22:48 +0200

    feature: register buttons click listener (#143)

---
+++ packages/source-control-worker/src/parts/HandleClickSourceControlButtons/HandleClickSourceControlButtons.ts
@@ -0,0 +1,5 @@
+import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
+
+export const handleClickSourceControlButtons = async (state: SourceControlState, clientx: number, clientY: number, name: string): Promise<SourceControlState> => {
+  return state
+}
