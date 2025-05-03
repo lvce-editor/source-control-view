fb12b186797ccb129066bee50a7fdd797ce1d3aa
Le Vivilet <levivlet@pm.me>
2025-05-03 11:54:52 +0200

    feature: add wheel command

---
+++ packages/source-control-worker/src/parts/HandleWheel/HandleWheel.ts
@@ -0,0 +1,5 @@
+import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
+
+export const handleWheel = async (state: SourceControlState, deltaMode: number, deltaY: number): Promise<SourceControlState> => {
+  return state
+}
