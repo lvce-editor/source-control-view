24b1a9b926e47b817a816a9b1465b25521d31212
Le Vivilet <levivlet@pm.me>
2025-05-03 23:34:13 +0200

    feature: add source actions test

---
+++ packages/source-control-worker/test/RequestSourceActions.test.ts
@@ -0,0 +1,38 @@
+import { expect, jest, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as ExtensionHost from '../src/parts/ExtensionHost/ExtensionHost.ts'
+import * as ExtensionMeta from '../src/parts/ExtensionMeta/ExtensionMeta.ts'
+import { requestSourceActions } from '../src/parts/RequestSourceActions/RequestSourceActions.ts'
+
+test('requestSourceActions', async () => {
+  const mockExtensions = [
+    {
+      'source-control-actions': {
+        action1: 'value1',
+        action2: 'value2',
+      },
+    },
+    {
+      'source-control-actions': {
+        action3: 'value3',
+      },
+    },
+  ]
+  const mockInvoke = jest.fn(() => {
+    return mockExtensions
+  })
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: mockInvoke,
+  })
+
+  ExtensionHost.set(mockRpc)
+
+  const result = await requestSourceActions()
+
+  expect(result).toEqual({
+    action1: 'value1',
+    action2: 'value2',
+    action3: 'value3',
+  })
+})
