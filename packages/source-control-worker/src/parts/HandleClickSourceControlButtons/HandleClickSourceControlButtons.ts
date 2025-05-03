b1c41300abc495b3065ef48d74a792848d13065a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 22:56:09 +0200

    feature: execute button command on click (#146)

--- packages/source-control-worker/src/parts/HandleClickSourceControlButtons/HandleClickSourceControlButtons.ts
+++ packages/source-control-worker/src/parts/HandleClickSourceControlButtons/HandleClickSourceControlButtons.ts
@@ -1,5 +1,17 @@
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
+import * as ExtensionHostCommand from '../ExtensionHostCommand/ExtensionHostCommand.ts'
 
 export const handleClickSourceControlButtons = async (state: SourceControlState, index: number, name: string): Promise<SourceControlState> => {
+  const { visibleItems } = state
+  const item = visibleItems[index]
+  if (!item) {
+    return state
+  }
+  const button = item.buttons.find((button) => button.label === name)
+  if (!button) {
+    return state
+  }
+  const file = item.file
+  await ExtensionHostCommand.executeCommand(button.command, file)
   return state
 }
