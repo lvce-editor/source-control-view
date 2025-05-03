7ff27e3abd54e96fe7253fa9442a057baa501973
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 12:19:07 +0200

    feature: add new iconTheme api (#133)

--- packages/source-control-worker/src/parts/RendererWorkerApi/RendererWorkerApi.ts
+++ packages/source-control-worker/src/parts/RendererWorkerApi/RendererWorkerApi.ts
@@ -12,6 +12,7 @@ export interface RendererWorkerApi {
   readonly 'Focus.setFocus': (focusKey: number) => Promise<void>
   readonly 'IconTheme.getFileIcon': (options: any) => Promise<string>
   readonly 'IconTheme.getFolderIcon': (options: any) => Promise<string>
+  readonly 'IconTheme.getIcons': (request: readonly any[]) => Promise<readonly string[]>
   readonly 'Main.openUri': (uri: string, focus?: boolean, props?: any) => Promise<void>
   readonly 'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker': (port: MessagePort, initialCommand: string, rpcId: number) => Promise<void>
   readonly 'Viewlet.closeWidget': (id: number) => Promise<void>
