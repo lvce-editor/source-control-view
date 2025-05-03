fdb60c46fc0bb39cfaa5d2e21acfbb8831937e51
Le Vivilet <levivlet@pm.me>
2025-05-03 22:58:55 +0200

    feature: improve refresh command

--- packages/source-control-worker/src/parts/Refresh/Refresh.ts
+++ packages/source-control-worker/src/parts/Refresh/Refresh.ts
@@ -1,5 +1,38 @@
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
+import { getDisplayItems } from '../GetDisplayItems/GetDisplayItems.ts'
+import * as GetFinalDeltaY from '../GetFinalDeltaY/GetFinalDeltaY.ts'
+import { getGroups } from '../GetGroups/GetGroups.ts'
+import { getListHeight } from '../GetListHeight/GetListHeight.ts'
+import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.ts'
+import { getVisibleSourceControlItems } from '../GetVisibleSourceControlItems/GetVisibleSourceControlItems.ts'
+import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
 
 export const refresh = async (state: SourceControlState): Promise<SourceControlState> => {
-  return state
+  const { itemHeight, height, minimumSliderSize, fileIconCache, enabledProviderIds, splitButtonEnabled, actionsCache } = state
+  const { allGroups, gitRoot } = await getGroups(enabledProviderIds)
+  const isExpanded = true
+  const displayItems = getDisplayItems(allGroups, isExpanded)
+  const total = displayItems.length
+  const contentHeight = total * itemHeight
+  const listHeight = getListHeight(total, itemHeight, height)
+  const scrollBarHeight = ScrollBarFunctions.getScrollBarSize(height, contentHeight, minimumSliderSize)
+  const numberOfVisible = GetNumberOfVisibleItems.getNumberOfVisibleItems(listHeight, itemHeight)
+  const minLineY = 0
+  const maxLineY = Math.min(numberOfVisible, total)
+  const visibleItems = getVisibleSourceControlItems(displayItems, minLineY, maxLineY, actionsCache, fileIconCache)
+  const finalDeltaY = GetFinalDeltaY.getFinalDeltaY(listHeight, itemHeight, total)
+  return {
+    ...state,
+    allGroups,
+    gitRoot,
+    items: displayItems,
+    visibleItems,
+    enabledProviderIds,
+    isExpanded,
+    splitButtonEnabled,
+    maxLineY,
+    scrollBarHeight,
+    finalDeltaY,
+    actionsCache,
+  }
 }
