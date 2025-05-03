5e18c419c3ab9947e2b24a5c7ae983bc8a09a9b0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 12:03:10 +0200

    feature: add setDeltaY command (#131)

--- packages/source-control-worker/src/parts/HandleWheel/HandleWheel.ts
+++ packages/source-control-worker/src/parts/HandleWheel/HandleWheel.ts
@@ -1,5 +1,6 @@
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
+import { setDeltaY } from '../SetDeltaY/SetDeltaY.ts'
 
 export const handleWheel = async (state: SourceControlState, deltaMode: number, deltaY: number): Promise<SourceControlState> => {
-  return state
+  return setDeltaY(state, state.deltaY + deltaY)
 }
