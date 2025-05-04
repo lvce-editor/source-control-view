84548671bd5ec9a0ce3c5f6c3a2d0bd4e7570f87
Le Vivilet <levivlet@pm.me>
2025-05-04 09:33:18 +0200

    lint

--- packages/source-control-worker/test/RenderEventListeners.test.ts
+++ packages/source-control-worker/test/RenderEventListeners.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@jest/globals'
-import { renderEventListeners } from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 import * as DomEventListenersFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import { renderEventListeners } from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 
 test('renderEventListeners returns correct event listeners', () => {
   const listeners = renderEventListeners()
