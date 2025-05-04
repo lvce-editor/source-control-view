7f39b36f27bbeffa8decbe13bef67907ea42d5aa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-04 09:33:37 +0200

    feature: add initialize test (#175)

--- packages/source-control-worker/test/Initialize.test.ts
+++ packages/source-control-worker/test/Initialize.test.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
+import * as RpcRegistry from '@lvce-editor/rpc-registry'
 import { initialize } from '../src/parts/Initialize/Initialize.ts'
+import * as ParentRpc from '../src/parts/ParentRpc/ParentRpc.ts'
 import * as RpcId from '../src/parts/RpcId/RpcId.ts'
-import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
 
-test.skip('initialize', async () => {
-  // @ts-ignore
+test('initialize', async () => {
   const mockRpc = MockRpc.create({
     commandMap: {},
+    invoke() {},
     invokeAndTransfer: (method: string, ...params: any[]) => {
       return Promise.resolve()
     },
   })
-  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+  ParentRpc.set(mockRpc)
   await initialize()
   const actual = RpcRegistry.get(RpcId.ExtensionHostWorker)
   expect(actual).toBeDefined()
