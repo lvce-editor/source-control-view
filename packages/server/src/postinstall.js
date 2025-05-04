919831b72e2dc996476fc25fb1016b63887268e6
Le Vivilet <levivlet@pm.me>
2025-05-04 11:16:11 +0200

    feature: update postinstall

--- packages/server/src/postinstall.js
+++ packages/server/src/postinstall.js
@@ -1,38 +1,38 @@
-// import { readdir, readFile, writeFile } from 'node:fs/promises'
-// import { dirname, join } from 'node:path'
-// import { fileURLToPath, pathToFileURL } from 'node:url'
+import { readdir, readFile, writeFile } from 'node:fs/promises'
+import { dirname, join } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
 
-// const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-// const root = join(__dirname, '..', '..', '..')
+const root = join(__dirname, '..', '..', '..')
 
-// export const getRemoteUrl = (path) => {
-//   const url = pathToFileURL(path).toString().slice(8)
-//   return `/remote/${url}`
-// }
+export const getRemoteUrl = (path) => {
+  const url = pathToFileURL(path).toString().slice(8)
+  return `/remote/${url}`
+}
 
-// const nodeModulesPath = join(root, 'packages', 'server', 'node_modules')
+const nodeModulesPath = join(root, 'packages', 'server', 'node_modules')
 
-// const fileSearchWorkerPath = join(root, '.tmp', 'dist', 'dist', 'fileSearchWorkerMain.js')
+const fileSearchWorkerPath = join(root, '.tmp', 'dist', 'dist', 'fileSearchWorkerMain.js')
 
-// const serverStaticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
+const serverStaticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
 
-// const RE_COMMIT_HASH = /^[a-z\d]+$/
-// const isCommitHash = (dirent) => {
-//   return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
-// }
+const RE_COMMIT_HASH = /^[a-z\d]+$/
+const isCommitHash = (dirent) => {
+  return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
+}
 
-// const dirents = await readdir(serverStaticPath)
-// const commitHash = dirents.find(isCommitHash) || ''
-// const rendererWorkerMainPath = join(serverStaticPath, commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+const dirents = await readdir(serverStaticPath)
+const commitHash = dirents.find(isCommitHash) || ''
+const rendererWorkerMainPath = join(serverStaticPath, commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
 
-// const content = await readFile(rendererWorkerMainPath, 'utf-8')
-// const remoteUrl = getRemoteUrl(fileSearchWorkerPath)
-// if (!content.includes('// const fileSearchWorkerUrl = ')) {
-//   const occurrence = `const fileSearchWorkerUrl = \`\${assetDir}/packages/file-search-worker/dist/fileSearchWorkerMain.js\``
-//   const replacement = `// const fileSearchWorkerUrl = \`\${assetDir}/packages/file-search-worker/dist/fileSearchWorkerMain.js\`
-//   const fileSearchWorkerUrl = \`${remoteUrl}\``
+const content = await readFile(rendererWorkerMainPath, 'utf-8')
+const remoteUrl = getRemoteUrl(fileSearchWorkerPath)
+if (!content.includes('// const sourceControlWorkerUrl = ')) {
+  const occurrence = `const sourceControlWorkerUrl = \`\${assetDir}/packages/source-control-worker/dist/sourceControlWorkerMain.js\``
+  const replacement = `// const sourceControlWorkerUrl = \`\${assetDir}/packages/source-control-worker/dist/sourceControlWorkerMain.js\`
+  const sourceControlWorkerUrl = \`${remoteUrl}\``
 
-//   const newContent = content.replace(occurrence, replacement)
-//   await writeFile(rendererWorkerMainPath, newContent)
-// }
+  const newContent = content.replace(occurrence, replacement)
+  await writeFile(rendererWorkerMainPath, newContent)
+}
