69f53e6f452a47b088ac5efe91045683d946f074
Le Vivilet <levivlet@pm.me>
2025-05-04 20:32:30 +0200

    test

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -13,6 +13,7 @@ process.env.PATH_PREFIX = '/source-control-view'
 const { commitHash } = await sharedProcess.exportStatic({
   root,
   extensionPath: '',
+  testPath: 'packages/e2e',
 })
 
 const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
