989cf7c7291b3920b0a7a026df9e5a554692a054
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-04 17:30:34 +0200

    feature: add stage e2e test (#194)

---
+++ packages/e2e/src/source-control.stage.ts
@@ -0,0 +1,26 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'source-control.stage'
+
+export const skip = 1
+
+export const test: Test = async ({ Locator, expect, QuickPick, Command, SideBar, FileSystem, Workspace, Extension }) => {
+  // arrange
+  const uri = new URL('../fixtures/sample-source-control-provider', import.meta.url).toString()
+  await Extension.addWebExtension(uri)
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Source Control')
+
+  // act
+  await Command.execute(`sampleSourceControl.stage`, `${tmpDir}/test.css`)
+
+  // assert
+  // const sourceControlView = Locator('.Viewlet.SourceControl')
+  // await expect(sourceControlView).toBeVisible()
+  // const treeItems = Locator('.SourceControlItems .TreeItem')
+  // await expect(treeItems).toHaveCount(2)
+  // await expect(treeItems.nth(0)).toHaveText('Changes1')
+  // await expect(treeItems.nth(1)).toHaveText('test.css')
+}
