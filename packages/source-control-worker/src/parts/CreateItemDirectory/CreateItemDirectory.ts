c49071aa2795da2b696a98fa5ba702a8b84f07b4
Le Vivilet <levivilet@pm.me>
2025-09-07 17:18:58 +0200

    feature: simplify aria role import

--- packages/source-control-worker/src/parts/CreateItemDirectory/CreateItemDirectory.ts
+++ packages/source-control-worker/src/parts/CreateItemDirectory/CreateItemDirectory.ts
@@ -1,6 +1,6 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles } from '@lvce-editor/virtual-dom-worker'
 import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
-import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as GetBadgeVirtualDom from '../GetBadgeVirtualDom/GetBadgeVirtualDom.ts'
--- packages/source-control-worker/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
+++ packages/source-control-worker/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
@@ -1,5 +1,5 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import { AriaRoles } from '@lvce-editor/virtual-dom-worker'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getIconVirtualDom = (icon: string, type = VirtualDomElements.Div): VirtualDomNode => {
