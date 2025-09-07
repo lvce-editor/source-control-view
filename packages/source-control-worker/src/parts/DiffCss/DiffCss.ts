86e7e0eaf3c6726e8ed5509ea35f1cb5291caf4f
Le Vivilet <levivilet@pm.me>
2025-09-07 22:46:08 +0200

    fix: css diff

--- packages/source-control-worker/src/parts/DiffCss/DiffCss.ts
+++ packages/source-control-worker/src/parts/DiffCss/DiffCss.ts
@@ -1,5 +1,5 @@
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
 
 export const isEqual = (oldState: SourceControlState, newState: SourceControlState): boolean => {
-  return newState.inputBoxHeight === newState.inputBoxHeight
+  return oldState.inputBoxHeight === newState.inputBoxHeight
 }
