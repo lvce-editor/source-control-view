5c4fb12b8d0d4aa7695cc30136c5083e0da39bb0
Le Vivilet <levivilet@pm.me>
2025-09-07 17:55:53 +0200

    feature: simplify import

--- packages/source-control-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/source-control-worker/src/parts/RenderItems/RenderItems.ts
@@ -1,8 +1,9 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
 import * as GetSourceControlDom from '../GetSourceControlVirtualDom/GetSourceControlVirtualDom.ts'
 
 export const renderItems = (oldState: SourceControlState, newState: SourceControlState): any => {
   const visible = newState.visibleItems
   const dom = GetSourceControlDom.getSourceControlVirtualDom(visible, newState.splitButtonEnabled)
-  return ['Viewlet.setDom2', dom]
+  return [ViewletCommand.SetDom2, dom]
 }
