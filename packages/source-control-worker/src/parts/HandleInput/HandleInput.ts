22ff5581073aec0dfa8450f857ccee7771df3f46
Le Vivilet <levivilet@pm.me>
2025-09-07 22:13:12 +0200

    feature: make input height computation more dynamic

--- packages/source-control-worker/src/parts/HandleInput/HandleInput.ts
+++ packages/source-control-worker/src/parts/HandleInput/HandleInput.ts
@@ -1,11 +1,10 @@
 import { InputSource } from '@lvce-editor/constants'
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
+import { getInputHeight } from '../GetInputHeight/GetInputHeight.ts'
 
 export const handleInput = async (state: SourceControlState, value: string, inputSource = InputSource.User): Promise<SourceControlState> => {
-  const lines = value.split('\n')
-  const lineCount = lines.length
-  const lineHeight = 30
-  const inputBoxHeight = lineHeight * lineCount
+  const { width } = state
+  const inputBoxHeight = await getInputHeight(value, width)
   return {
     ...state,
     inputValue: value,
