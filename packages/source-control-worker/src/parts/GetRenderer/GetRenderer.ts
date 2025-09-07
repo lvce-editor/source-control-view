8ae74f7f71b90ef4883b298d7ad5d2280535f0b2
Le Vivilet <levivilet@pm.me>
2025-09-07 21:11:43 +0200

    feature: add renderValue

--- packages/source-control-worker/src/parts/GetRenderer/GetRenderer.ts
+++ packages/source-control-worker/src/parts/GetRenderer/GetRenderer.ts
@@ -1,11 +1,14 @@
 import type { Renderer } from '../Renderer/Renderer.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
+import { renderValue } from '../RenderValue/RenderValue.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
     case DiffType.RenderItems:
       return RenderItems.renderItems
+    case DiffType.RenderValue:
+      return renderValue
     default:
       throw new Error('unknown renderer')
   }
