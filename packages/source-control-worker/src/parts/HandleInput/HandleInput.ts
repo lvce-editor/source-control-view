7a4ee45d8444be40562cf3a463b712f13c2e89a5
Le Vivilet <levivilet@pm.me>
2025-09-07 19:15:10 +0200

    feature: add handleInput (#254)

---
+++ packages/source-control-worker/src/parts/HandleInput/HandleInput.ts
@@ -0,0 +1,5 @@
+import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
+
+export const handleInput = async (state: SourceControlState): Promise<SourceControlState> => {
+  return state
+}
