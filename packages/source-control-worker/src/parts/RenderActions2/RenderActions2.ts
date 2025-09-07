19aeb9c90d54fa0d6fc184e53337be53bc3b8ae9
Le Vivilet <levivilet@pm.me>
2025-09-07 21:52:04 +0200

    feature: register renderActions command

--- packages/source-control-worker/src/parts/RenderActions2/RenderActions2.ts
+++ packages/source-control-worker/src/parts/RenderActions2/RenderActions2.ts
@@ -1,7 +1,8 @@
+import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.ts'
 
-export const renderActions = (uid: number): readonly VirtualDomNode[] => {
+export const renderActions = (state: SourceControlState): readonly VirtualDomNode[] => {
   const actions: readonly any[] = []
   const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
   return dom
