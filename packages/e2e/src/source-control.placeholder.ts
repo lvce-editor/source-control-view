7c92e0dbeb83753f02eee6e3dfff075ad5166196
Le Vivilet <levivilet@pm.me>
2025-09-07 19:21:24 +0200

    feature: add placeholder e2e test (#258)

---
+++ packages/e2e/src/source-control.placeholder.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'source-control.placeholder'
+
+export const test: Test = async ({ Locator, expect, QuickPick, Command, SideBar, FileSystem, Workspace, Extension }) => {
+  // arrange
+  const uri = new URL('../fixtures/sample-source-control-provider', import.meta.url).toString()
+  await Extension.addWebExtension(uri)
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+
+  // act
+  await SideBar.open('Source Control')
+
+  // assert
+  const sourceControlView = Locator('.Viewlet.SourceControl')
+  await expect(sourceControlView).toBeVisible()
+  const treeItems = Locator('.SourceControlItems .TreeItem')
+  await expect(treeItems).toHaveCount(2)
+  await expect(treeItems.nth(0)).toHaveText('Changes1')
+  await expect(treeItems.nth(1)).toHaveText('test.css')
+}
