9ab9615551bbe3cfc74bc8c31386888196be929d
Le Vivilet <levivilet@pm.me>
2025-09-07 17:12:33 +0200

    lint

--- packages/source-control-worker/src/parts/ExtensionHost/ExtensionHost.ts
+++ packages/source-control-worker/src/parts/ExtensionHost/ExtensionHost.ts
@@ -1,4 +1,4 @@
-import type { MockRpc } from '@lvce-editor/rpc-registry';
+import type { MockRpc } from '@lvce-editor/rpc-registry'
 import { ExtensionHost } from '@lvce-editor/rpc-registry'
 
 export const { invoke, set, registerMockRpc } = ExtensionHost
