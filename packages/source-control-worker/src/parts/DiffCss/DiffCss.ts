8af2bd56740db11eb9232d04e86721c7c40c26e5
Le Vivilet <levivilet@pm.me>
2025-09-07 22:08:28 +0200

    feature: improve css diff

--- packages/source-control-worker/src/parts/DiffCss/DiffCss.ts
+++ packages/source-control-worker/src/parts/DiffCss/DiffCss.ts
@@ -1,6 +1,5 @@
-import { InputSource } from '@lvce-editor/constants'
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
 
 export const isEqual = (oldState: SourceControlState, newState: SourceControlState): boolean => {
-  return newState.inputSource === InputSource.User || oldState.inputValue === newState.inputValue
+  return newState.inputBoxHeight === newState.inputBoxHeight
 }
