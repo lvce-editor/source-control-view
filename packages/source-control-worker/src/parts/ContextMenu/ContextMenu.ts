3983b33df149d9eecf8fb711403ffd96993211f8
Le Vivilet <levivilet@pm.me>
2025-09-07 17:47:11 +0200

    feature: simplify import

--- packages/source-control-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/source-control-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -1,5 +1,5 @@
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const show = async (x: number, y: number, id: number, ...args: readonly any[]): Promise<void> => {
-  return ParentRpc.invoke('ContextMenu.show', x, y, id, ...args)
+  return RendererWorker.showContextMenu(x, y, id, ...args)
 }
