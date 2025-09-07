1281b48a6e5be1dcdfada76ac06bd0e13a56dc98
Le Vivilet <levivilet@pm.me>
2025-09-07 21:58:47 +0200

    feature: add newline e2e test

---
+++ packages/e2e/src/source-control.handle-new-line.ts
@@ -0,0 +1,23 @@
+import { type Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'source-control.handle-new-line'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, Locator, expect, SideBar, FileSystem, Workspace, Extension }) => {
+  // arrange
+  const uri = new URL('../fixtures/sample-source-control-provider', import.meta.url).toString()
+  await Extension.addWebExtension(uri)
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Source Control')
+
+  // act
+  await Command.execute('Source Control.handleInput', '\n', 2)
+
+  // assert
+  const input = Locator(`.SourceControl .InputBox`)
+  await expect(input).toBeVisible()
+  await expect(input).toHaveValue('\n')
+  await expect(input).toHaveCSS('height', '60px')
+}
