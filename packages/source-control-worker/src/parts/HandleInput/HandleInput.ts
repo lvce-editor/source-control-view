7cd7257a7538914ac683a526a7bf2fb0f8507ec9
Le Vivilet <levivilet@pm.me>
2025-09-07 22:07:57 +0200

    feature: update inputBox height on input (#276)

--- packages/source-control-worker/src/parts/HandleInput/HandleInput.ts
+++ packages/source-control-worker/src/parts/HandleInput/HandleInput.ts
@@ -2,9 +2,14 @@ import { InputSource } from '@lvce-editor/constants'
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
 
 export const handleInput = async (state: SourceControlState, value: string, inputSource = InputSource.User): Promise<SourceControlState> => {
+  const lines = value.split('\n')
+  const lineCount = lines.length
+  const lineHeight = 30
+  const inputBoxHeight = lineHeight * lineCount
   return {
     ...state,
     inputValue: value,
     inputSource,
+    inputBoxHeight,
   }
 }
