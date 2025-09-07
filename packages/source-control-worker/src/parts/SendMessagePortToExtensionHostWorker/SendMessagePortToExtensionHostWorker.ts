0ffb1086187625ded45f1ea67385fb0b38329d07
Le Vivilet <levivilet@pm.me>
2025-09-07 17:17:23 +0200

    feature: simplify rpc id import

--- packages/source-control-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
+++ packages/source-control-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
@@ -1,5 +1,5 @@
+import { RpcId } from '@lvce-editor/rpc-registry'
 import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
-import * as RpcId from '../RpcId/RpcId.ts'
 
 export const sendMessagePortToExtensionHostWorker = async (port: MessagePort): Promise<void> => {
   const command = 'HandleMessagePort.handleMessagePort2'
