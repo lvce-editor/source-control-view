26abee699c1bda2d6581367d2ea371e162c5b6c2
Le Vivilet <levivilet@pm.me>
2025-09-07 18:59:17 +0200

    feature: improve rename e2e test

--- packages/e2e/src/source-control.renamed-file.ts
+++ packages/e2e/src/source-control.renamed-file.ts
@@ -11,16 +11,15 @@ export const test: Test = async ({ SourceControl, Locator, expect, Command, Side
   await Command.execute('ExtensionHost.executeCommand', `sampleSourceControl.rename`, `${tmpDir}/test.css`, `${tmpDir}/test-renamed.css`)
   await Workspace.setPath(tmpDir)
   await SideBar.open('Source Control')
-  await SourceControl.handleClickSourceControlButtons(1, 'Stage')
 
   // act
-  await SourceControl.handleClickSourceControlButtons(1, 'Unstage Changes')
+  await SourceControl.handleClickSourceControlButtons(1, 'Stage')
 
-  //  assert
+  // assert
   const sourceControlView = Locator('.Viewlet.SourceControl')
   await expect(sourceControlView).toBeVisible()
   const treeItems = Locator('.SourceControlItems .TreeItem')
   await expect(treeItems).toHaveCount(2)
-  await expect(treeItems.nth(0)).toHaveText('Changes1')
+  await expect(treeItems.nth(0)).toHaveText('Staged Changes1')
   await expect(treeItems.nth(1)).toHaveText('test-renamed.css')
 }
