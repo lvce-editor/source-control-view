eb77b7611b5ea3584c1c0f5f6b3f753691a0b63f
Le Vivilet <levivilet@pm.me>
2025-09-07 19:22:53 +0200

    feature: improve placeholder e2e test (#259)

--- packages/e2e/src/source-control.placeholder.ts
+++ packages/e2e/src/source-control.placeholder.ts
@@ -7,17 +7,13 @@ export const test: Test = async ({ Locator, expect, QuickPick, Command, SideBar,
   const uri = new URL('../fixtures/sample-source-control-provider', import.meta.url).toString()
   await Extension.addWebExtension(uri)
   const tmpDir = await FileSystem.getTmpDir()
-  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
   await Workspace.setPath(tmpDir)
 
   // act
   await SideBar.open('Source Control')
 
   // assert
-  const sourceControlView = Locator('.Viewlet.SourceControl')
-  await expect(sourceControlView).toBeVisible()
-  const treeItems = Locator('.SourceControlItems .TreeItem')
-  await expect(treeItems).toHaveCount(2)
-  await expect(treeItems.nth(0)).toHaveText('Changes1')
-  await expect(treeItems.nth(1)).toHaveText('test.css')
+  const input = Locator(`.SourceControl .InputBox`)
+  await expect(input).toBeVisible()
+  await expect(input).toHaveAttribute(`placeholder`, `Message (Enter) to commit on 'master'`)
 }
