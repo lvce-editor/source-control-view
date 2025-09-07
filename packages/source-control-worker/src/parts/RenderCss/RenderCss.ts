0bb85dce1446947a51b21baa0185a2869eb5f55a
Le Vivilet <levivilet@pm.me>
2025-09-07 22:07:54 +0200

    feature: render css input height from state

--- packages/source-control-worker/src/parts/RenderCss/RenderCss.ts
+++ packages/source-control-worker/src/parts/RenderCss/RenderCss.ts
@@ -2,7 +2,10 @@ import { ViewletCommand } from '@lvce-editor/constants'
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
 
 export const renderCss = (oldState: SourceControlState, newState: SourceControlState): any => {
-  const { id } = newState
-  const css = `` // TODO
+  const { id, inputBoxHeight } = newState
+  const css = `:root {
+  --SourceControlInputHeight: ${inputBoxHeight}px;
+}
+`
   return [ViewletCommand.SetCss, id, css]
 }
