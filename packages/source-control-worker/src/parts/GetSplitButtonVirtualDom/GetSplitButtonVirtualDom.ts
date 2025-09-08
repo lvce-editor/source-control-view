d3ba1ab75642efac00f6a90e9edd6c7a7f71de91
Le Vivilet <levivilet@pm.me>
2025-09-08 10:10:58 +0200

    feature: use mergeclassNames function

--- packages/source-control-worker/src/parts/GetSplitButtonVirtualDom/GetSplitButtonVirtualDom.ts
+++ packages/source-control-worker/src/parts/GetSplitButtonVirtualDom/GetSplitButtonVirtualDom.ts
@@ -1,6 +1,7 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getSplitButtonVirtualDom = (hasItems: boolean, splitButtonEnabled: boolean, buttonText: string): readonly VirtualDomNode[] => {
@@ -10,12 +11,12 @@ export const getSplitButtonVirtualDom = (hasItems: boolean, splitButtonEnabled: 
   return [
     {
       type: VirtualDomElements.Div,
-      className: `${ClassNames.SplitButton} ${hasItems ? '' : ClassNames.SplitButtonDisabled}`,
+      className: MergeClassNames.mergeClassNames(ClassNames.SplitButton, hasItems ? '' : ClassNames.SplitButtonDisabled),
       childCount: 3,
     },
     {
       type: VirtualDomElements.Div,
-      className: `${ClassNames.SplitButtonContent} ${hasItems ? '' : ClassNames.SplitButtonContentDisabled}`,
+      className: MergeClassNames.mergeClassNames(ClassNames.SplitButtonContent, hasItems ? '' : ClassNames.SplitButtonContentDisabled),
       childCount: 1,
       tabIndex: 0,
     },
@@ -27,13 +28,13 @@ export const getSplitButtonVirtualDom = (hasItems: boolean, splitButtonEnabled: 
     },
     {
       type: VirtualDomElements.Div,
-      className: `${ClassNames.SplitButtonDropDown} ${hasItems ? '' : ClassNames.SplitButtonDropDownDisabled}`,
+      className: MergeClassNames.mergeClassNames(ClassNames.SplitButtonDropDown, hasItems ? '' : ClassNames.SplitButtonDropDownDisabled),
       childCount: 1,
       tabIndex: 0,
     },
     {
       type: VirtualDomElements.Div,
-      className: `${ClassNames.MaskIcon} ${ClassNames.MaskIconChevronDown}`,
+      className: MergeClassNames.mergeClassNames(ClassNames.MaskIcon, ClassNames.MaskIconChevronDown),
       childCount: 0,
     },
   ]
