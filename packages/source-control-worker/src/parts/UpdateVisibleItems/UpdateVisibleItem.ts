4d7e2c83768f463e7145712e8148bb36ec80834a
Le Vivilet <levivilet@pm.me>
2025-09-10 20:57:24 +0200

    tsc

--- packages/source-control-worker/src/parts/UpdateVisibleItems/UpdateVisibleItem.ts
+++ packages/source-control-worker/src/parts/UpdateVisibleItems/UpdateVisibleItem.ts
@@ -5,8 +5,8 @@ import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOf
 import { getVisibleSourceControlItems } from '../GetVisibleSourceControlItems/GetVisibleSourceControlItems.ts'
 
 export const updateVisibleItems = async (state: SourceControlState, expandedGroups: Record<string, boolean>): Promise<SourceControlState> => {
-  const { itemHeight, height, actionsCache, fileIconCache, allGroups } = state
-  const displayItems = getDisplayItems(allGroups, expandedGroups)
+  const { itemHeight, height, actionsCache, fileIconCache, allGroups, iconDefinitions } = state
+  const displayItems = getDisplayItems(allGroups, expandedGroups, iconDefinitions)
   const total = displayItems.length
   const listHeight = getListHeight(total, itemHeight, height)
   const numberOfVisible = GetNumberOfVisibleItems.getNumberOfVisibleItems(listHeight, itemHeight)
