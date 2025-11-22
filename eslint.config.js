24a4b42858943f264d930a3e28a593548b2468a6
Le Vivilet <levivilet@pm.me>
2025-11-22 16:17:20 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import config from '@lvce-editor/eslint-config'
+import actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config,
+  ...actions,
   {
     files: ['**/*.ts'],
     rules: {
