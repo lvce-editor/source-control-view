1faedba1a7c18871bb0c2341b0ca9297cbcd815b
Le Vivilet <levivilet@pm.me>
2025-11-25 16:16:16 +0100

    feature: remove unused import (#360)

--- packages/source-control-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/source-control-worker/src/parts/DiffItems/DiffItems.ts
@@ -1,7 +1,4 @@
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
-import * as DiffType from '../DiffType/DiffType.ts'
-
-export const diffType = DiffType.RenderItems
 
 export const isEqual = (oldState: SourceControlState, newState: SourceControlState): boolean => {
   return (
