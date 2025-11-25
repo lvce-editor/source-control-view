532d3ed0aa4a74f51b99d934a4ed8775ec530198
Le Vivilet <levivilet@pm.me>
2025-11-25 16:36:42 +0100

    feature: use import meta resolve

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -9,4 +9,4 @@ export const instantiationsPath = join(root, 'packages', 'source-control-worker'
 
 export const workerPath = join(root, '.tmp/dist/dist/sourceControlWorkerMain.js')
 
-export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
+export const playwrightPath = import.meta.resolve('../../e2e/node_modules/playwright/index.mjs')
