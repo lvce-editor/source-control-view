566a839eb4780c3a42f1cfcbd1dff2491ef3ae75
Le Vivilet <levivilet@pm.me>
2025-11-25 17:43:11 +0100

    feature: add menu entry types (#402)

--- packages/source-control-worker/src/parts/GetMenuEntries2/GetMenuEntries2.ts
+++ packages/source-control-worker/src/parts/GetMenuEntries2/GetMenuEntries2.ts
@@ -1,5 +1,7 @@
+import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
+import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
 import { getMenuEntries } from '../GetMenuEntries/GetMenuEntries.ts'
 
-export const getMenuEntries2 = (): readonly any[] => {
+export const getMenuEntries2 = (state: SourceControlState): readonly MenuEntry[] => {
   return getMenuEntries()
 }
