1fcbb80e04195b85f9cc1925b6985764d5ee082d
Le Vivilet <levivilet@pm.me>
2025-11-25 17:00:52 +0100

    fix

--- packages/source-control-worker/test/Preferences.test.ts
+++ packages/source-control-worker/test/Preferences.test.ts
@@ -1,7 +1,12 @@
 import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { get } from '../src/parts/Preferences/Preferences.ts'
 
-test('get', () => {
-  const result = get('test')
+test('get', async (): Promise<void> => {
+  const commandMap = {
+    'Preferences.get': (): Promise<any> => Promise.resolve(false),
+  }
+  RendererWorker.registerMockRpc(commandMap)
+  const result = await get('test')
   expect(result).toBe(false)
 })
