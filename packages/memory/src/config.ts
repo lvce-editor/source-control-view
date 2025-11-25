9b74a9c59c75a33f9f40c78a0dae86d0c487ad04
Le Vivilet <levivilet@pm.me>
2025-11-25 17:52:04 +0100

    feature: reduce typescript memory (#406)

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,7 +3,7 @@ import { root } from './root.ts'
 
 export const threshold = 440_000
 
-export const instantiations = 7000
+export const instantiations = 3000
 
 export const instantiationsPath = join(root, 'packages', 'source-control-worker')
 
