0a899629d8ea96950ac1f5e7e599dcc82e7dcb3a
Le Vivilet <levivilet@pm.me>
2025-11-25 17:11:15 +0100

    feature: remove unused file

--- packages/source-control-worker/src/parts/ParentRpc/ParentRpc.ts
+++
@@ -1,5 +0,0 @@
-import { RendererWorker, type MockRpc } from '@lvce-editor/rpc-registry'
-
-const { invoke, invokeAndTransfer, set, registerMockRpc } = RendererWorker
-
-export { invoke, invokeAndTransfer, set, registerMockRpc, type MockRpc }
