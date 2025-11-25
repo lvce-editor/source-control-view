e3d7e61c9e6ade25e0cfce39a1806406c5a6bcdd
Le Vivilet <levivilet@pm.me>
2025-11-25 17:28:20 +0100

    feature: use readonly types

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,4 @@
 import config from '@lvce-editor/eslint-config'
 import actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [
-  ...config,
-  ...actions,
-  {
-    files: ['**/*.ts'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
-      'prefer-destructuring': 'off',
-      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-      'no-restricted-syntax': 'off',
-      'jest/no-restricted-jest-methods': 'off',
-      'unicorn/prefer-export-from': 'off',
-    },
-  },
-]
+export default [...config, ...actions]
