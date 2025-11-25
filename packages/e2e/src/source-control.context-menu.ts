0fd9a25c5e46401a65f8389f671dbdd044a70f81
Le Vivilet <levivilet@pm.me>
2025-11-25 17:35:09 +0100

    feature: add context menu e2e test

---
+++ packages/e2e/src/source-control.context-menu.ts
@@ -0,0 +1,28 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'source-control.context-menu'
+
+export const test: Test = async ({ Command, Locator, expect, SourceControl, FileSystem, Workspace, Extension }) => {
+  // arrange
+  const uri = new URL('../fixtures/sample-source-control-provider', import.meta.url).toString()
+  await Extension.addWebExtension(uri)
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SourceControl.show()
+
+  // act
+  await Command.execute('Source Control.handleContextMenu', 0, 0, 0)
+
+  // assert
+  const menu = Locator('.Menu')
+  await expect(menu).toBeVisible()
+  const menuItems = menu.locator('.MenuItem')
+  await expect(menuItems).toHaveCount(8)
+  const menuItem1 = menuItems.nth(0)
+  await expect(menuItem1).toHaveText('Open Changes')
+  const menuItem2 = menuItems.nth(1)
+  await expect(menuItem2).toHaveText('Open File')
+  const menuItem3 = menuItems.nth(2)
+  await expect(menuItem3).toHaveText('Open File (HEAD)')
+}
