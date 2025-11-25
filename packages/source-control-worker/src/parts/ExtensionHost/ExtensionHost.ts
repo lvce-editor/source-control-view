76f06551f97edeee23105c609a74e6535642a630
Le Vivilet <levivilet@pm.me>
2025-11-25 16:37:32 +0100

    feature: remove unused file (#368)

--- packages/source-control-worker/src/parts/ExtensionHost/ExtensionHost.ts
+++
@@ -1,6 +0,0 @@
-import type { MockRpc } from '@lvce-editor/rpc-registry'
-import { ExtensionHost } from '@lvce-editor/rpc-registry'
-
-export const { invoke, set, registerMockRpc } = ExtensionHost
-
-export type { MockRpc }
