4e5fa75a2a56d32c05df2e81a66f04c86848b47f
Le Vivilet <levivilet@pm.me>
2025-11-25 16:55:06 +0100

    feature: query preferences from renderer worker

--- packages/source-control-worker/src/parts/Preferences/Preferences.ts
+++ packages/source-control-worker/src/parts/Preferences/Preferences.ts
@@ -1,3 +1,5 @@
-export const get = (key: string): boolean => {
-  return false
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+
+export const get = (key: string): Promise<any> => {
+  return RendererWorker.getPreference(key)
 }
