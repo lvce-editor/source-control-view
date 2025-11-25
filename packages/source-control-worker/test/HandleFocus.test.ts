3945f18c04cc8c5cef7794fc52b1635b5bb77934
Le Vivilet <levivilet@pm.me>
2025-11-25 16:49:42 +0100

    feature: add focus test (#372)

---
+++ packages/source-control-worker/test/HandleFocus.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { handleFocus } from '../src/parts/HandleFocus/HandleFocus.ts'
+
+test('handleFocus - returns state unchanged', async (): Promise<void> => {
+  const state = createDefaultState()
+  const result = await handleFocus(state)
+  expect(result).toBe(state)
+})
