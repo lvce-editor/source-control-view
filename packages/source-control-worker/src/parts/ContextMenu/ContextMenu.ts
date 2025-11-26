26ff08c7735ad360341764fee510cc6526b4028f
Le Vivilet <levivilet@pm.me>
2025-11-26 12:47:00 +0100

    feature: remove unused code

--- packages/source-control-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/source-control-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -1,10 +1,6 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { ContextMenuProps } from '../ContextMenuProps/ContextMenuProps.ts'
 
-export const show = async (x: number, y: number, id: number, ...args: readonly any[]): Promise<void> => {
-  return RendererWorker.showContextMenu(x, y, id, ...args)
-}
-
 export const show2 = async <T extends ContextMenuProps>(uid: number, menuId: ContextMenuProps['menuId'], x: number, y: number, args: ContextMenuProps): Promise<void> => {
   await RendererWorker.showContextMenu2(uid, menuId, x, y, args)
 }
