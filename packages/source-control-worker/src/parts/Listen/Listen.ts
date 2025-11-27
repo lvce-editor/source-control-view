2debaa2e5d2718e2c6ef9bd035b3107c17906d63
Le Vivilet <levivilet@pm.me>
2025-11-27 12:12:50 +0100

    test

--- packages/source-control-worker/src/parts/Listen/Listen.ts
+++ packages/source-control-worker/src/parts/Listen/Listen.ts
@@ -1,5 +1,5 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import { RendererWorker, TextMeasurementWorker } from '@lvce-editor/rpc-registry'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import { registerCommands } from '../SourceControlStates/SourceControlStates.ts'
 
