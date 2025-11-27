b3ef880d22d30b66f8a07a195da568d61ad69293
Le Vivilet <levivilet@pm.me>
2025-11-27 13:06:21 +0100

    feature: rename function (#433)
    * feature: rename function
    
    * rename

--- packages/source-control-worker/test/HandleFocus.test.ts
+++ packages/source-control-worker/test/HandleFocus.test.ts
@@ -1,10 +1,10 @@
 import { expect, test } from '@jest/globals'
 import type { SourceControlState } from '../src/parts/SourceControlState/SourceControlState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
-import { handleFocus } from '../src/parts/HandleFocus/HandleFocus.ts'
+import { handleInputFocus } from '../src/parts/HandleFocus/HandleFocus.ts'
 
 test('handleFocus - returns state unchanged', async (): Promise<void> => {
   const state: SourceControlState = createDefaultState()
-  const result = await handleFocus(state)
+  const result = await handleInputFocus(state)
   expect(result).toBe(state)
 })
