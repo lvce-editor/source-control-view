221e0daa9ce2ab317d01531b0fed928d77794255
Le Vivilet <levivilet@pm.me>
2025-11-27 13:05:25 +0100

    feature: diff focus context

--- packages/source-control-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/source-control-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,8 +1,9 @@
 import * as DiffCss from '../DiffCss/DiffCss.ts'
+import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffItems.isEqual, DiffValue.isEqual, DiffCss.isEqual]
+export const modules = [DiffItems.isEqual, DiffValue.isEqual, DiffCss.isEqual, DiffFocus.isEqual]
 
-export const numbers = [DiffType.RenderItems, DiffType.RenderValue, DiffType.RenderCss]
+export const numbers = [DiffType.RenderItems, DiffType.RenderValue, DiffType.RenderCss, DiffType.RenderFocus]
