0e90f1309043b4241a5d25fefdf8baefc7f36f2a
Le Vivilet <levivilet@pm.me>
2025-11-27 13:29:12 +0100

    feature: register acceptInput command (#457)

--- packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/source-control-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,4 +1,5 @@
 import { terminate } from '@lvce-editor/viewlet-registry'
+import { acceptInput } from '../AcceptInput/AcceptInput.ts'
 import * as Create2 from '../Create2/Create2.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
 import * as GetInfo from '../GetInfo/GetInfo.ts'
@@ -36,6 +37,7 @@ import * as ViewAsTree from '../ViewAsTree/ViewAsTree.ts'
 
 export const commandMap = {
   'Initialize.initialize': Initialize.initialize,
+  'SourceControl.acceptInput': WrapCommand.wrapCommand(acceptInput),
   'SourceControl.create2': Create2.create2,
   'SourceControl.diff2': Diff2.diff2,
   'SourceControl.getCommandIds': WrapCommand.getCommandIds,
