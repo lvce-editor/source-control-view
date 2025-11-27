6e26dc72e77f280e1e03f29044e0c26db6cfaec8
Le Vivilet <levivilet@pm.me>
2025-11-27 13:03:49 +0100

    feature: rename function

--- packages/source-control-worker/src/parts/HandleFocus/HandleFocus.ts
+++ packages/source-control-worker/src/parts/HandleFocus/HandleFocus.ts
@@ -1,5 +1,5 @@
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
 
-export const handleFocus = async (state: SourceControlState): Promise<SourceControlState> => {
+export const handleInputFocus = async (state: SourceControlState): Promise<SourceControlState> => {
   return state
 }
