e44fa360c9a5c1e760ed96e77037c39c868df293
Le Vivilet <levivilet@pm.me>
2025-12-04 13:46:32 +0100

    tsc

--- packages/source-control-worker/src/parts/RenderActions2/RenderActions2.ts
+++ packages/source-control-worker/src/parts/RenderActions2/RenderActions2.ts
@@ -5,11 +5,7 @@ import { getActions } from '../GetActions/GetActions.ts'
 import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.ts'
 
 export const renderActions = (state: SourceControlState): readonly VirtualDomNode[] => {
-<<<<<<< HEAD
-  const actions: readonly Action[] = []
-=======
-  const actions: readonly any[] = getActions()
->>>>>>> origin/main
+  const actions: readonly Action[] = getActions()
   const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
   return dom
 }
