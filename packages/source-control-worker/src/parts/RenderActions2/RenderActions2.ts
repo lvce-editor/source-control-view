4d09aad03dea3fa0f94e0ac61d35d2f8e9a2aa14
Le Vivilet <levivilet@pm.me>
2025-12-04 13:42:55 +0100

    feature: improve renderactions

--- packages/source-control-worker/src/parts/RenderActions2/RenderActions2.ts
+++ packages/source-control-worker/src/parts/RenderActions2/RenderActions2.ts
@@ -1,9 +1,10 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { SourceControlState } from '../SourceControlState/SourceControlState.ts'
+import { getActions } from '../GetActions/GetActions.ts'
 import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.ts'
 
 export const renderActions = (state: SourceControlState): readonly VirtualDomNode[] => {
-  const actions: readonly any[] = []
+  const actions: readonly any[] = getActions()
   const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
   return dom
 }
