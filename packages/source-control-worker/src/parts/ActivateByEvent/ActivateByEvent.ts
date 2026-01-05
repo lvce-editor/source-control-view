b87f87546305f70545b086824004dab438a25dfe
Le Vivilet <levivilet@pm.me>
2026-01-05 21:31:29 +0100

    feature: validate arguments

--- packages/source-control-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
+++ packages/source-control-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
@@ -1,5 +1,9 @@
+import * as Assert from '@lvce-editor/assert'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const activateByEvent = (event: string, assetDir: string, platform: number): Promise<void> => {
+  Assert.string(event)
+  Assert.string(assetDir)
+  Assert.number(platform)
   return RendererWorker.activateByEvent(event, assetDir, platform)
 }
