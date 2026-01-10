d0792f98fea9da84f0adfd7dfec6c04f67106b2b
Le Vivilet <levivilet@pm.me>
2026-01-10 22:50:49 +0100

    feature: initialize extension management worker (#497)

--- packages/source-control-worker/src/parts/Initialize/Initialize.ts
+++ packages/source-control-worker/src/parts/Initialize/Initialize.ts
@@ -1,9 +1,10 @@
 import { ExtensionHost, TextMeasurementWorker } from '@lvce-editor/rpc-registry'
 import { createExtensionHostRpc } from '../CreateExtensionHostRpc/CreateExtensionHostRpc.ts'
 import { createTextMeasurementWorkerRpc } from '../CreateTextMeasurementWorkerRpc/CreateTextMeasurementWorkerRpc.ts'
+import { initializeExtensionManagementWorker } from '../InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts'
 
 export const initialize = async (): Promise<void> => {
-  const [extensionHostRpc, textRpc] = await Promise.all([createExtensionHostRpc(), createTextMeasurementWorkerRpc()])
+  const [extensionHostRpc, textRpc] = await Promise.all([createExtensionHostRpc(), createTextMeasurementWorkerRpc(), initializeExtensionManagementWorker()])
   ExtensionHost.set(extensionHostRpc)
   TextMeasurementWorker.set(textRpc)
 }
