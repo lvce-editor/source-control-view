66a967aa12e22205eb319c2fc0922af56a38f480
Le Vivilet <levivilet@pm.me>
2026-07-10 13:03:43 +0000

    feature: render source control button icons

---
+++ packages/e2e/src/source-control.commit-sync-lock-icon.ts
@@ -0,0 +1,18 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'source-control.commit-sync-lock-icon'
+
+export const test: Test = async ({ expect, Extension, FileSystem, Locator, SourceControl, Workspace }) => {
+  // arrange
+  const uri = import.meta.resolve('../fixtures/sample-source-control-provider')
+  await Extension.addWebExtension(uri)
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
+
+  // act
+  await SourceControl.show()
+
+  // assert
+  const lockIcon = Locator('.SplitButtonContent[name="Commit & Sync"] .MaskIconLock')
+  await expect(lockIcon).toHaveCount(1)
+}
