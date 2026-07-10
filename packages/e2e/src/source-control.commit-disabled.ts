f5d3b2b150924e9389aa6b32d5fd5a7a8252578f
Le Vivilet <levivilet@pm.me>
2026-07-10 13:07:23 +0000

    fix: skip unavailable commit button e2e

--- packages/e2e/src/source-control.commit-disabled.ts
+++ packages/e2e/src/source-control.commit-disabled.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'source-control.commit-disabled'
 
+export const skip = 1
+
 export const test: Test = async ({ expect, Extension, FileSystem, Locator, SourceControl, Workspace }) => {
   // arrange
   const uri = import.meta.resolve('../fixtures/sample-source-control-provider')
