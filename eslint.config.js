8c475dce7cb1ddafc7695795636e714536b9a54d
Le Vivilet <levivilet@pm.me>
2026-07-13 11:16:22 +0200

    chore: update eslint and eslint config to latest (#521)
    * chore: update eslint and eslint config to latest
    
    * fix: update ci and package lock

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,12 @@
 import config from '@lvce-editor/eslint-config'
 import actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [...config, ...actions]
+export default [
+  ...config,
+  ...actions,
+  {
+    rules: {
+      'e2e/prefer-execute-extension-command': 'off',
+    },
+  },
+]
