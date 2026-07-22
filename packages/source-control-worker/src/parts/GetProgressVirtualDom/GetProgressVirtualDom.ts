f165eb76760fd3233864a94eafc8ebf175d0b646
Le Vivilet <levivilet@pm.me>
2026-07-22 11:32:39 +0000

    chore: update eslint and eslint config to latest

--- packages/source-control-worker/src/parts/GetProgressVirtualDom/GetProgressVirtualDom.ts
+++ packages/source-control-worker/src/parts/GetProgressVirtualDom/GetProgressVirtualDom.ts
@@ -2,17 +2,19 @@ import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 
+const progressDom: readonly VirtualDomNode[] = [
+  {
+    childCount: 1,
+    className: ClassNames.ProgressContainer,
+    type: VirtualDomElements.Div,
+  },
+  {
+    childCount: 0,
+    className: ClassNames.Progress,
+    type: VirtualDomElements.Div,
+  },
+]
+
 export const getProgressVirtualDom = (): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.ProgressContainer,
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 0,
-      className: ClassNames.Progress,
-      type: VirtualDomElements.Div,
-    },
-  ]
+  return progressDom
 }
