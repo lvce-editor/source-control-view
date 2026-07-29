66aaceeeeecedba72e43db72e7a8302d73d3e539
Le Vivilet <levivilet@pm.me>
2026-07-29 12:56:47 +0000

    fix: hoist static virtual dom nodes

--- packages/source-control-worker/src/parts/GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts
+++ packages/source-control-worker/src/parts/GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts
@@ -4,17 +4,19 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
+const scrollBarNode: VirtualDomNode = {
+  childCount: 1,
+  className: MergeClassNames.mergeClassNames(ClassNames.ScrollBar, ClassNames.ScrollBarSmall),
+  onPointerDown: DomEventListenerFunctions.HandleScrollBarPointerDown,
+  type: VirtualDomElements.Div,
+}
+
 export const getScrollBarVirtualDom = (scrollBarHeight: number, scrollBarActive: boolean): readonly VirtualDomNode[] => {
   if (scrollBarHeight <= 0) {
     return []
   }
   return [
-    {
-      childCount: 1,
-      className: MergeClassNames.mergeClassNames(ClassNames.ScrollBar, ClassNames.ScrollBarSmall),
-      onPointerDown: DomEventListenerFunctions.HandleScrollBarPointerDown,
-      type: VirtualDomElements.Div,
-    },
+    scrollBarNode,
     {
       childCount: 0,
       className: MergeClassNames.mergeClassNames(ClassNames.ScrollBarThumb, scrollBarActive ? ClassNames.ScrollBarThumbActive : ''),
