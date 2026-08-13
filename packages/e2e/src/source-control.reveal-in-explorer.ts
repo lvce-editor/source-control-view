508e4f7c3f66936437533af3a51b019d9c22a69d
Le Vivilet <levivilet@pm.me>
2026-08-13 20:33:03 +0000

    fix: target source control item context menu

--- packages/e2e/src/source-control.reveal-in-explorer.ts
+++ packages/e2e/src/source-control.reveal-in-explorer.ts
@@ -14,7 +14,7 @@ export const test: Test = async ({ ContextMenu, expect, Extension, FileSystem, L
   await expect(changedFile).toBeVisible()
 
   // act
-  await SourceControl.handleContextMenu(2, 1, 100)
+  await changedFile.click({ button: 'right' })
   await ContextMenu.selectItem('Reveal in Explorer View')
 
   // assert
