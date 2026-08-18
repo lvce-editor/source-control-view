2954cec4e7246a8343df2d9e51752e521d2ac77e
Le Vivilet <levivilet@pm.me>
2026-08-18 12:50:12 -0700

    fix: replace source control dom during loading changes (#563)

--- packages/source-control-worker/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/source-control-worker/src/parts/RenderIncremental/RenderIncremental.ts
@@ -3,6 +3,9 @@ import type { SourceControlState } from '../SourceControlState/SourceControlStat
 import { renderItems } from '../RenderItems/RenderItems.ts'
 
 export const renderIncremental = (oldState: SourceControlState, newState: SourceControlState): any => {
+  if (oldState.loading !== newState.loading || oldState.workspacePath !== newState.workspacePath) {
+    return renderItems(oldState, newState)
+  }
   const oldDom = renderItems(oldState, oldState)[2]
   const newDom = renderItems(newState, newState)[2]
   const patches = diffTree(oldDom, newDom)
