92167bf420921639a2d89d100a3cc94aec0af567
Le Vivilet <levivilet@pm.me>
2026-08-22 14:52:33 +0000

    feature: contribute source control settings

--- packages/build/src/build.ts
+++ packages/build/src/build.ts
@@ -70,5 +70,6 @@ packageJson.main = 'dist/sourceControlWorkerMain.js'
 
 await writeJson(join(dist, 'package.json'), packageJson)
 
+await cp(join(root, 'packages', 'source-control-worker', 'settings.json'), join(dist, 'dist', 'settings.json'))
 await cp(join(root, 'README.md'), join(dist, 'README.md'))
 await cp(join(root, 'LICENSE'), join(dist, 'LICENSE'))
