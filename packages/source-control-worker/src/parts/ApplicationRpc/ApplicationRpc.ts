9362155aec127a63ec180893d2bc6132838d8f69
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2026-09-05 09:21:04 +0000

    fix: format application rpc routing

--- packages/source-control-worker/src/parts/ApplicationRpc/ApplicationRpc.ts
+++ packages/source-control-worker/src/parts/ApplicationRpc/ApplicationRpc.ts
@@ -1,7 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const invoke = async (applicationId: string | undefined, method: string, ...args: readonly unknown[]): Promise<any> => {
-  return applicationId === undefined
-    ? RendererWorker.invoke(method, ...args)
-    : RendererWorker.invoke('Application.execute', applicationId, method, ...args)
+  return applicationId === undefined ? RendererWorker.invoke(method, ...args) : RendererWorker.invoke('Application.execute', applicationId, method, ...args)
 }
