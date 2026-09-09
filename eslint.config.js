5a632dda416c0198013b99820f050777ce423ad7
Le Vivilet <levivilet@pm.me>
2026-09-09 15:20:07 +0200

    feature: enable regex rules (#573)

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import * as config from '@lvce-editor/eslint-config'
 export default defineConfig([
   ...config.default,
   ...config.recommendedVirtualDom,
+  ...config.recommendedRegex,
   ...config.recommendedActions,
   {
     rules: {
