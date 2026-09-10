fb7494ec7cf64d7d20b523edf5bf3341a148b28e
Le Vivilet <levivilet@pm.me>
2026-09-10 12:07:41 +0200

    feature: move inline diff editor breakpoint into state (#591)
    * feature: move inline diff editor breakpoint into state
    
    * feature: pass breakpoint in direct diff editor tests
    
    * feature: keep breakpoint test parameters readonly

--- packages/source-control-worker/src/parts/OpenDiffEditor/OpenDiffEditor.ts
+++ packages/source-control-worker/src/parts/OpenDiffEditor/OpenDiffEditor.ts
@@ -1,8 +1,6 @@
 import * as OpenUri from '../OpenUri/OpenUri.ts'
 
-const inlineDiffEditorBreakpoint = 800
-
-export const openDiffEditor = async (before: string, afterPath: string, width: number, applicationId?: string): Promise<void> => {
+export const openDiffEditor = async (before: string, afterPath: string, width: number, inlineDiffEditorBreakpoint: number, applicationId?: string): Promise<void> => {
   // TODO handle error
   // TODO should only pass uris to diff editor, diff editor should then resolve file contents
   if (width < inlineDiffEditorBreakpoint) {
