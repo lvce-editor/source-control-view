63eb469924b953b585eb1c013211c708fb25a366
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-10 09:06:48 +0000

    fix: update message rendering expectation

--- packages/source-control-worker/test/RenderItems.test.ts
+++ packages/source-control-worker/test/RenderItems.test.ts
@@ -126,11 +126,12 @@ test('renderItems - shows unavailable message instead of commit controls without
     1,
     [
       expect.objectContaining({ childCount: 1 }),
-      expect.objectContaining({
+      {
         childCount: 1,
-        paddingLeft: '20px',
+        className: ClassNames.Message,
         paddingRight: '20px',
-      }),
+        type: VirtualDomElements.Div,
+      },
       expect.objectContaining({
         text: 'No source control extensions are installed.',
         type: VirtualDomElements.Text,
