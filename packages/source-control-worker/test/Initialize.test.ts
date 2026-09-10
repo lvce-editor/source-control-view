dc9114a52a452050bfc52f61f108114ea71be57c
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-10 07:37:54 +0000

    feature: lazily connect to text measurement worker

--- packages/source-control-worker/test/Initialize.test.ts
+++ packages/source-control-worker/test/Initialize.test.ts
@@ -1,16 +1,18 @@
-import { expect, test } from '@jest/globals'
+import { expect, jest, test } from '@jest/globals'
 import { ExtensionHost, ExtensionManagementWorker, RendererWorker, TextMeasurementWorker } from '@lvce-editor/rpc-registry'
 import { initialize } from '../src/parts/Initialize/Initialize.ts'
 
 test('initialize should set ExtensionManagementWorker, ExtensionHost and TextMeasurementWorker RPCs', async (): Promise<void> => {
+  const sendTextMeasurementPort = jest.fn(async (): Promise<void> => {})
   const commandMap = {
     'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionManagementWorker': async (): Promise<void> => {},
-    'SendMessagePortToExtensionHostWorker.sendMessagePortToTextMeasurementWorker': async (): Promise<void> => {},
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToTextMeasurementWorker': sendTextMeasurementPort,
   }
   RendererWorker.registerMockRpc(commandMap)
 
   await initialize()
 
+  expect(sendTextMeasurementPort).not.toHaveBeenCalled()
   expect(typeof ExtensionManagementWorker.invoke).toBe('function')
   expect(typeof ExtensionHost.invoke).toBe('function')
   expect(typeof TextMeasurementWorker.invoke).toBe('function')
