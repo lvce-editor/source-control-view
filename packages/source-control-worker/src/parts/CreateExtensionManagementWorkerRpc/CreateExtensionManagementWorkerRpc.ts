ea89b4df9b299584fdced892d3dc428c1dcd6dca
Le Vivilet <levivilet@pm.me>
2026-09-10 09:34:26 +0200

    feature: lazily connect to extension management worker (#580)

--- packages/source-control-worker/src/parts/CreateExtensionManagementWorkerRpc/CreateExtensionManagementWorkerRpc.ts
+++ packages/source-control-worker/src/parts/CreateExtensionManagementWorkerRpc/CreateExtensionManagementWorkerRpc.ts
@@ -1,10 +1,10 @@
-import { type Rpc, TransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { type Rpc, LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { VError } from '@lvce-editor/verror'
 
 export const createExtensionManagementWorkerRpc = async (): Promise<Rpc> => {
   try {
-    const rpc = await TransferMessagePortRpcParent.create({
+    const rpc = await LazyTransferMessagePortRpcParent.create({
       commandMap: {},
       send: (port) => RendererWorker.sendMessagePortToExtensionManagementWorker(port, 0),
     })
