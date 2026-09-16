91861730a5c2ff4cc70eb248cd3637cbbd47d68c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2026-09-16 03:01:36 +0000

    fix: avoid extra source control indent binding

--- packages/source-control-worker/src/parts/GetTreeItemIndent/GetTreeItemIndent.ts
+++ packages/source-control-worker/src/parts/GetTreeItemIndent/GetTreeItemIndent.ts
@@ -1,11 +1,10 @@
 import { DirentType } from '@lvce-editor/constants'
 
 const defaultIndent = 16 // 1rem = 16px
-const groupIndent = 4
 
 export const getTreeItemIndent = (type: number): number => {
   if (type === DirentType.Directory || type === DirentType.DirectoryExpanded) {
-    return groupIndent
+    return 4
   }
   return defaultIndent
 }
